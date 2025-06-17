cff3789c5b869f65b09a548837f4bc956c8fec68
Diego Lopez <diegolopez@meta.com>
2025-06-17 12:16:34 -0700

    Finished the create board modal

---
+++ frontend/src/components/NewBoardForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export default function NewBoardForm({
+  createModalClass,
+  setCreateModalClass,
+}) {
+  const [boardName, setBoardName] = useState("");
+  const [boardCategory, setBoardCategory] = useState("celebration");
+  const [boardAuthor, setBoardAuthor] = useState("");
+
+  return (
+    <section className={"modal " + createModalClass}>
+      <div className="modal-content">
+        <div className="close-container">
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+        <h2>Create New Board</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            //TODO submit logic
+          }}
+        >
+          <label>
+            Board name
+            <input
+              type="text"
+              value={boardName}
+              onChange={(e) => setBoardName(e.target.value)}
+            />
+          </label>
+          <label>
+            Category Type
+            <select
+              name="category"
+              value={boardCategory}
+              onChange={(e) => setBoardCategory(e.target.value)}
+            >
+              <option value="celebration">Celebration</option>
+              <option value="thank-you">Thank You</option>
+              <option value="inspiration">Inspiration</option>
+            </select>
+          </label>
+          <label>
+            Board Author
+            <input
+              type="text"
+              value={boardAuthor}
+              onChange={(e) => setBoardAuthor(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </section>
+  );
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,14 +2,22 @@ import Header from "../components/Header";
 import HomeNav from "../components/HomeNav";
 import BoardsContainer from "../components/BoardsContainer";
 import Footer from "../components/Footer";
+import NewBoardForm from "../components/NewBoardForm";
+import { useState } from "react";
 
 function HomePage() {
+  const [createModalClass, setCreateModalClass] = useState("closed");
+
   return (
     <main>
       <Header />
       <HomeNav />
       <BoardsContainer />
       <Footer />
+      <NewBoardForm
+        createModalClass={createModalClass}
+        setCreateModalClass={setCreateModalClass}
+      />
     </main>
   );
 }
